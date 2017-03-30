b86936533796da02d5298d480b581d19aff1e25d
Jason Rahm <jason.rahm@ucdenver.edu>
2017-03-30 15:07:14 -0600

    photo gallery demo complete

---
+++ dev/photo-gallery/gallery-scripts.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+  
+  var currentThumb, selectedThumb, selectedFull, selectedPre;
+  var backdrop = '<div class="backdrop"></div>';
+  var firstImage = $('.gallery-thumbnails img')[0];
+  var fadeTimerShort = 250;
+  var fadeTimerMedium = 500;
+  var overlay = '<div class="overlay"></div>';
+  var lightboxClose = '<span class="lightbox-close">X</span>';
+  
+  function setImage(x) {
+    selectedThumb = $(x).attr('src');
+    selectedFull = selectedThumb.replace('_thumbnail', '_fullsize');
+    selectedPre = selectedThumb.replace('_thumbnail', '_preview');
+  }
+  
+  function placeImage() {
+    $('.gallery-preview').html('<img alt ="" src="' + selectedPre + '"><span class="view-full-image">View Full Image</span>');    
+    $('.lightbox-content').html('<img alt ="" src="' + selectedFull + '">' + lightboxClose);
+  }
+  
+  //Click function that selects image when clicked on
+  $('.gallery-thumbnails img').click(function () { 
+    
+    if(currentThumb !== this) {
+      $('.gallery-thumbnails img').removeClass('selected');
+      $(this).addClass('selected');
+      
+      setImage(this);
+
+      $('.gallery-preview').fadeOut(fadeTimerShort, function() {
+        setTimeout(placeImage(), fadeTimerShort);
+        $('.gallery-preview').fadeIn(fadeTimerShort);
+      });  
+
+      currentThumb = this;
+    }
+    
+  });
+  
+  //Set Default Image on Page Load
+  function setDefaultImage() {
+    setImage(firstImage);
+    placeImage(firstImage);
+    $(firstImage).addClass('selected');
+    currentThumb = firstImage;
+  }
+  
+  setDefaultImage();
+  
+  //Display full image in lightbox
+    //http://jsfiddle.net/MrGarretto/97dns8u5/
+    //https://codepen.io/anon/pen/dvQEGj
+    //http://jsfiddle.net/d6DH6/7/
+
+  $('.gallery-preview').click(function() {
+    $('body').append(overlay);
+    $('.lightbox').css('display', 'block');
+    $('.overlay').animate({"opacity": "0.7"}, fadeTimerMedium);
+    $('.lightbox-content').animate({"opacity": "1.0"}, fadeTimerMedium);
+    $('.overlay, .lightbox-close').click(function() {
+      $('.overlay, .lightbox-content').animate({"opacity": "0.0"}, fadeTimerMedium, function() {
+        $('.lightbox').css("display", "none");
+        $('.overlay').remove();
+      });
+    });
+  });
+
+});

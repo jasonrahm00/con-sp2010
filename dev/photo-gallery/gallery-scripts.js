05e558821d66fb0163c49b104cbeb7108be106b9
Jason Rahm <jason.rahm@ucdenver.edu>
2017-03-31 13:53:59 -0600

    previous/next button functionality added

--- dev/photo-gallery/gallery-scripts.js
+++ dev/photo-gallery/gallery-scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   
-  var caption, currentThumb, selectedThumb, selectedFull, selectedPre;
+  var caption, currentThumb, selectedIndex, selectedFull, selectedPre, selectedThumb;
   var backdrop = '<div class="backdrop"></div>';
   var firstImage = $('.gallery-thumbnails img')[0];
   var fadeTimerShort = 250;
@@ -8,45 +8,71 @@ $(document).ready(function() {
   var overlay = '<div class="overlay"></div>';
   var lightboxClose = '<span class="lightbox-close">X</span>';
   
+  //Called in the setImage function If image has alt text, use that as a caption, else no caption shows
   function getCaption(y) {
-    if(y !== "") {
-      return '<span class="image-caption">' + $(y).attr('alt') + '</span>';
+    if($(y).attr('alt') !== '') {
+      return '<figcaption class="image-caption">' + $(y).attr('alt') + '</figcaption>';
     } else {
       return '';
     } 
   }
   
+  //Called in the setImage function for later use in the next/previous buttons
+  function setIndex(y) {
+    return $('.gallery-thumbnails img').index(y);
+  }
+  
+  //Hide/show controls depending on the index of the current image
+  function indexCheck() {
+    if(selectedIndex === 0) {
+      $('.previous-image').addClass('inactive');
+    } else {
+      $('.previous-image').removeClass('inactive');
+    }
+    
+    if((selectedIndex + 1) === $('.gallery-thumbnails img').length) {
+      $('.next-image').addClass('inactive');
+    } else {
+      $('.next-image').removeClass('inactive');
+    }
+  }
+  
+  //Set the selected image when a thumbnail is clicked
   function setImage(x) {
+    selectedIndex = setIndex(x);
+    indexCheck();
     caption = getCaption(x);
     selectedThumb = $(x).attr('src');
     selectedFull = selectedThumb.replace('-thumb', '-full');
     selectedPre = selectedThumb.replace('-thumb', '-pre');
   }
   
+  //Place selected image preview and full versions in their respective containers  
   function placeImage() {
-    $('.preview-wrapper').html('<img alt ="" src="' + selectedPre + '">' + caption + '<span class="view-full-image">View Full Image</span>');    
-    $('.lightbox-content').html('<img alt ="" src="' + selectedFull + '">' + lightboxClose);
+    $('.preview-wrapper').html('<figure><img alt ="Image preview" src="' + selectedPre + '">' + caption + '</figure><span class="view-full-image"><img alt="Magnifying glass icon" src="/PublishingImages/UCDLayoutImages/searchButton.png"></span>');    
+    $('.lightbox-content').html('<img alt ="" src="' + selectedFull + '">' + caption + lightboxClose);
   }
   
+  function swapImage() {
+    $('.preview-wrapper').fadeOut(fadeTimerShort, function() {
+      setTimeout(placeImage(), fadeTimerShort);
+      $('.preview-wrapper').fadeIn(fadeTimerShort);
+    });
+    
+    $('.gallery-thumbnails img').removeClass('selected');
+    currentThumb = $('.gallery-thumbnails img')[selectedIndex];
+    $(currentThumb).addClass('selected');
+    
+  }
+
   //Click event that selects image when clicked on
   $('.gallery-thumbnails img').click(function () { 
     
     if(currentThumb !== this) {
-      $('.gallery-thumbnails img').removeClass('selected');
-      $(this).addClass('selected');
-      
       setImage(this);
-
-      $('.preview-wrapper').fadeOut(fadeTimerShort, function() {
-        setTimeout(placeImage(), fadeTimerShort);
-        $('.preview-wrapper').fadeIn(fadeTimerShort);
-      });  
-
-      currentThumb = this;
+      swapImage();
     }
-    
-    console.log($('.gallery-thumbnails img').index($(this)));
-    console.log(caption);
+
   });
   
   //Set Default Image on Page Load
@@ -60,10 +86,6 @@ $(document).ready(function() {
   setDefaultImage();
   
   //Display full image in lightbox
-    //http://jsfiddle.net/MrGarretto/97dns8u5/
-    //https://codepen.io/anon/pen/dvQEGj
-    //http://jsfiddle.net/d6DH6/7/
-
   $('.preview-wrapper').click(function() {
     $('body').append(overlay);
     $('.lightbox').css('display', 'block');
@@ -75,6 +97,20 @@ $(document).ready(function() {
         $('.overlay').remove();
       });
     });
+  });
+  
+  //Gallery controls
+  $('.gallery-controls span').click(function() {
+    
+    if($(this).hasClass('inactive')) return;
+    
+    if($(this).hasClass('previous-image')) {
+      setImage($('.gallery-thumbnails img')[selectedIndex - 1]);
+    }
+    if($(this).hasClass('next-image')) {
+      setImage($('.gallery-thumbnails img')[selectedIndex + 1]);
+    }
+    swapImage();
   });
 
 });

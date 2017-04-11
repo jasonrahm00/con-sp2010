20dbe309924450f76f9878bd5c185c347960f183
Jason Rahm <jason.rahm@ucdenver.edu>
2017-04-11 12:10:33 -0600

    added click event to document that will close tooltip

--- dev/tooltip-widget/tooltip-scripts.js
+++ dev/tooltip-widget/tooltip-scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   var currentTarget, topPosition;
   
+  $('.tooltip-target').each(function() {
+    topPosition = $(this).next().height() * 0.5;
+    $(this).next().css('top', '-' + topPosition + 'px');
+  });
+  
   $('.tooltip-target').click(function (){
     if(currentTarget == this) {
       $(this).next().removeClass('active');
@@ -8,8 +13,17 @@ $(document).ready(function() {
     } else {
       $('.tooltip-target').not(this).next().removeClass('active').hide();
       currentTarget = this;
-      topPosition = ($(currentTarget).next().height()) * 0.5;
-      $(currentTarget).next().css('top', '-' + topPosition + 'px').addClass('active');
+      $(currentTarget).next().show().addClass('active');
+    }
+  });
+  
+  $(document).click(function(event) {
+    var clickTarget = $(event.target);
+    if($(clickTarget).hasClass('tooltip-target') || $(clickTarget).hasClass('tooltip-content') || $(clickTarget).parents().hasClass('tooltip-content')) {
+      return;
+    } else {
+      $('.tooltip-content.active').removeClass('active').hide();
+      currentTarget = null;
     }
   });
   

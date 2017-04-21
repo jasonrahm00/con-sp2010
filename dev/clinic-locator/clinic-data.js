b4a8426c11bffd623cbfc1a61ffcc7ce99cb3227
Jason Rahm <jason.rahm@ucdenver.edu>
2017-04-21 15:06:02 -0600

    initial commit for clinic locator

---
+++ dev/clinic-locator/clinic-data.js
@@ -0,0 +1,651 @@
+var clinics = [
+  {
+    "name": "Belleview Point Clinic",
+    "street": "5001 S. Parker Rd. | Suite 215",
+    "city": "Aurora, CO",
+    "zip": 80015,
+    "mapUrl": "https://goo.gl/maps/DwoP1WDhDCx",
+    "lat": 39.6252013,
+    "long": -104.8228108,
+    "phone": "303-315-6200",
+    "email": "hcp.belleviewpoint@ucdenver.edu",
+    "pageUrl": "",
+    "region": "",
+    "serviceCategory": "",
+    "services": ["Primary Care", "Convenient Care", "Behavioral Health", "DOT Exams", "School Physicals"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "9:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Tuesday",
+        "open": "11:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "9:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Thursday",
+        "open": "11:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Friday",
+        "open": "9:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Anschutz Campus Health Center",
+    "street": "12348 E. Montview Blvd. | 2nd Floor",
+    "city": "Aurora, CO",
+    "zip": 80045,
+    "mapUrl": "https://goo.gl/maps/2tJeYvNkWoQ2",
+    "lat": 39.7470331,
+    "long": -104.8438491,
+    "phone": "303-724-6242",
+    "email": "con.chc@ucdenver.edu",
+    "pageUrl": "",
+    "region": "",
+    "serviceCategory": "",
+    "services": ["Behavioral and Counseling Services", "Flu Shots", "Physical and General Services"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "7:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Tuesday",
+        "open": "7:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "7:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Thursday",
+        "open": "7:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Friday",
+        "open": "7:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "9:00",
+        "close": "1:00",
+        "closed": false
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Anschutz Outpatient Pavilion",
+    "street": "1635 Aurora Ct.",
+    "city": "Aurora, CO",
+    "zip": 80045,
+    "mapUrl": "https://goo.gl/maps/cbjjF7sgi5P2",
+    "lat": 39.742571,
+    "long": -104.8394653,
+    "phone": "720-848-1700",
+    "email": "",
+    "pageUrl": "midwifery-center",
+    "region": "metro",
+    "serviceCategory": "Midwifery",
+    "services": ["Annual Gynecologic Exams", "Breastfeeding Consultation", "Family Planning", "Gynecologic Care", "Labor and Birth", "Labor Support", "Nitrous Oxide", "Preconception Counseling", "Prenatal Care", "Postpartum Care", "Vaginal Birth after Cesarean Section", "Water Birth"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Tuesday",
+        "open": "9:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Thursday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Friday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Lowry Clinic",
+    "street": "8111 East Lowry Blvd | Suite 120",
+    "city": "Denver, CO",
+    "zip": 80230,
+    "mapUrl": "https://goo.gl/maps/HvoHh9VQwvP2",
+    "lat": 39.7187081,
+    "long": -104.893519,
+    "phone": "720-848-1700",
+    "email": "",
+    "pageUrl": "midwifery-center",
+    "region": "metro",
+    "serviceCategory": "Midwifery",
+    "services": ["Annual Gynecologic Exams", "Breastfeeding Consultation", "Family Planning", "Gynecologic Care", "Labor and Birth", "Labor Support", "Nitrous Oxide", "Preconception Counseling", "Prenatal Care", "Postpartum Care", "Vaginal Birth after Cesarean Section", "Water Birth"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Tuesday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Thursday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Friday",
+        "open": "",
+        "close": "",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Lone Tree Health Center",
+    "street": "9548 Park Meadows Drive",
+    "city": "Lone Tree, CO",
+    "zip": 80124,
+    "mapUrl": "https://goo.gl/maps/bfXZMVAbvES2",
+    "lat": 39.5531462,
+    "long": -104.8784145,
+    "phone": "720-848-1700",
+    "email": "",
+    "pageUrl": "midwifery-center",
+    "region": "metro",
+    "serviceCategory": "Midwifery",
+    "services": ["Annual Gynecologic Exams", "Breastfeeding Consultation", "Family Planning", "Gynecologic Care", "Labor and Birth", "Labor Support", "Nitrous Oxide", "Preconception Counseling", "Prenatal Care", "Postpartum Care", "Vaginal Birth after Cesarean Section", "Water Birth"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Tuesday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Thursday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Friday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Sheridan Health Services Community Clinic",
+    "street": "3525 West Oxford Ave. | Unit G3",
+    "city": "Denver, CO",
+    "zip": 80236,
+    "mapUrl": "https://goo.gl/maps/abmfwEYtD8B2",
+    "lat": 39.642831,
+    "long": -105.033712,
+    "phone": "303-315-6150",
+    "email": "",
+    "pageUrl": "",
+    "region": "",
+    "serviceCategory": "",
+    "services": ["Adult Services", "Behavioral Health", "Case Management", "Dental Services", "Maternity Services"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Tuesday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "10:00",
+        "close": "6:00",
+        "closed": false
+      },
+      {
+        "day": "Thursday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Friday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Sheridan Health Services School-Based Clinic",
+    "street": "4107B S. Federal Blvd.",
+    "city": "Sheridan, CO",
+    "zip": 80110,
+    "mapUrl": "https://goo.gl/maps/W76tteaCNor",
+    "lat": 39.640554,
+    "long": -105.026819,
+    "phone": "303-315-6150",
+    "email": "",
+    "pageUrl": "",
+    "region": "",
+    "serviceCategory": "",
+    "services": ["Behavioral Health" ,"Children and Teen Services"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "10:00",
+        "close": "6:00",
+        "closed": false
+      },
+      {
+        "day": "Tuesday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "10:00",
+        "close": "6:00",
+        "closed": false
+      },
+      {
+        "day": "Thursday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Friday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "University Nurse Midwives",
+    "street": "1635 Aurora Ct. | Floor 3, Desk B",
+    "city": "Aurora, CO",
+    "zip": 80045,
+    "mapUrl": "https://goo.gl/maps/MVpuP55ihS12",
+    "lat": 39.742571,
+    "long": -104.8394653,
+    "phone": "720-848-1060",
+    "email": "",
+    "pageUrl": "midwifery-center",
+    "region": "",
+    "serviceCategory": "",
+    "services": ["Annual Gynecologic Exams", "Breastfeeding Consultation", "Family Planning", "Labor and Birth", "Labor Support", "Postpartum Care", "Preconception Counseling", "Prenatal Care", "Vaginal Birth after Cesarean Section"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Tuesday",
+        "open": "8:00",
+        "close": "7:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Thursday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Friday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Boulder Health Center",
+    "street": "5495 Arapahoe Ave",
+    "city": "Boulder, CO",
+    "zip": 80303,
+    "mapUrl": "https://goo.gl/maps/cZvaqSVZf6v",
+    "lat": 40.0150815,
+    "long": -105.226161,
+    "phone": "720-494-3128",
+    "email": "",
+    "pageUrl": "midwifery-center",
+    "region": "north",
+    "serviceCategory": "Midwifery",
+    "services": ["Annual Gynecologic Exams", "Breastfeeding Consultation", "Family Planning", "Gynecologic Care", "Labor and Birth", "Labor Support", "Nitrous Oxide", "Preconception Counseling", "Prenatal Care", "Postpartum Care", "Vaginal Birth after Cesarean Section", "Water Birth"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Tuesday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Wednesday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Thursday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Friday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Carbon Valley Health Center",
+    "street": "11083 Colorado Boulevard",
+    "city": "Firestone, CO",
+    "zip": 80504,
+    "mapUrl": "https://goo.gl/maps/5TrYUP5SPoC2",
+    "lat": 40.1613753,
+    "long": -104.9427151,
+    "phone": "720-494-3128",
+    "email": "",
+    "pageUrl": "midwifery-center",
+    "region": "north",
+    "serviceCategory": "Midwifery",
+    "services": ["Annual Gynecologic Exams", "Breastfeeding Consultation", "Family Planning", "Gynecologic Care", "Labor and Birth", "Labor Support", "Nitrous Oxide", "Preconception Counseling", "Prenatal Care", "Postpartum Care", "Vaginal Birth after Cesarean Section", "Water Birth"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Tuesday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Wednesday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Thursday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Friday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  },
+  {
+    "name": "Longmont Clinic",
+    "street": "1925 W. Mountain View Avenue",
+    "city": "Longmont, CO",
+    "zip": 80501,
+    "mapUrl": "https://goo.gl/maps/tqDng2gXUep",
+    "lat": 40.1809583,
+    "long": -105.1249203,
+    "phone": "720-494-3128",
+    "email": "",
+    "pageUrl": "midwifery-center",
+    "region": "north",
+    "serviceCategory": "Midwifery",
+    "services": ["Annual Gynecologic Exams", "Breastfeeding Consultation", "Family Planning", "Gynecologic Care", "Labor and Birth", "Labor Support", "Nitrous Oxide", "Preconception Counseling", "Prenatal Care", "Postpartum Care", "Vaginal Birth after Cesarean Section", "Water Birth"],
+    "daysOpen": [
+      {
+        "day": "Monday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Tuesday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Wednesday",
+        "open": "8:00",
+        "close": "5:00",
+        "closed": false
+      },
+      {
+        "day": "Thursday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Friday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Saturday",
+        "open": "",
+        "close": "",
+        "closed": true
+      },
+      {
+        "day": "Sunday",
+        "open": "",
+        "close": "",
+        "closed": true
+      }
+    ]
+  }
+];

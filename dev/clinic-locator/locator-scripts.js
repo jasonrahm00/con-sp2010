dfd720052923093b19e7b099fefce2d8fa62ee3d
Jason Rahm <jason.rahm@ucdenver.edu>
2017-04-25 13:57:34 -0600

    geo coordinates added to data attribute for each location

--- dev/clinic-locator/locator-scripts.js
+++ dev/clinic-locator/locator-scripts.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
   
   /*************************** Get Data from Sharepoint Table on Page ***************************/
   
-  var clinicData,
+  var clinicData, currentLatLong, infoWindow, map, mapBounds,
+      allLatLongs = [],
       clinics = $('table[summary="clinic-locations "] tr').not($('table[summary="clinic-locations "] tr.ms-viewheadertr.ms-vhltr'));
   
   function getData(tableRow) {
@@ -15,16 +16,14 @@ $(document).ready(function() {
       baseContent: $(tableRow).find('td.ms-vb2:nth-child(2)')[0].innerHTML,
       services: $(tableRow).find('td.ms-vb2:nth-child(3)')[0].innerHTML,
       hours: $(tableRow).find('td.ms-vb2:nth-child(4)')[0].innerHTML,
-      lat: $(tableRow).find('td.ms-vb2:nth-child(5)')[0].innerHTML,
-      long: $(tableRow).find('td.ms-vb2:nth-child(6)')[0].innerHTML
+      lat: $(tableRow).find('td.ms-vb2:nth-child(5)')[0].textContent,
+      long: $(tableRow).find('td.ms-vb2:nth-child(6)')[0].textContent
     }
   }
 
   /*************************** Map ***************************/
   
-  /*
   function initMap() {
-    var allLatLongs = [];
     
     map = new google.maps.Map(document.getElementById('clinicMap'), {
       zoom: 8,
@@ -33,13 +32,13 @@ $(document).ready(function() {
     
     $.each(clinics, function(index, value) {
       clinicData = getData(value);
-      var currentLatLong = new google.maps.LatLng(clinicData.lat, clinicData.long);
+      currentLatLong = new google.maps.LatLng(clinicData.lat, clinicData.long);
       setMarkers(map, clinicData, currentLatLong);
       allLatLongs.push(currentLatLong);
     });
     
     //Extend bounds of map and recenter so all markers are visible using gmaps api fitBounds method
-    var mapBounds = new google.maps.LatLngBounds();
+    mapBounds = new google.maps.LatLngBounds();
     allLatLongs.forEach(function(latLng) {
       mapBounds.extend(latLng);
     });
@@ -72,14 +71,15 @@ $(document).ready(function() {
   }
  
   initMap();
-  */
   
-  //Reformat data in table
+  /*************************** Reformat Table Datat to Display Better ***************************/
+  //Has to be done after map data is loaded, or map won't load
+  
   $(clinics).each(function () {
     
     clinicData = getData(this);
     
-    $(this).html('<td><section class="clinic-card"><h2>' + clinicData.name + '</h2><section class="location"><h3>Location</h3>' + clinicData.baseContent + '</section><section class="services"><h3>Services</h3>' + clinicData.services + '</section><section class="hours"><h3>Hours</h3>' + clinicData.hours + '</section></section></td>');
+    $(this).html('<td><section data-coords="{lat: ' + clinicData.lat + ',long: ' + clinicData.long + '}" class="clinic-card"><h2>' + clinicData.name + '</h2><section class="location"><h3>Location</h3>' + clinicData.baseContent + '</section><section class="services"><h3>Services</h3>' + clinicData.services + '</section><section class="hours"><h3>Hours</h3>' + clinicData.hours + '</section></section></td>');
     
   });
   

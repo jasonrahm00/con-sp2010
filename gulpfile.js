27debffbe458a354b9f8eab6319debd72387e625
Jason Rahm <jason.rahm@ucdenver.edu>
2017-05-15 15:59:54 -0600

    uglify js readded to compile-locator

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ gulp.task('pulse', function() {
 
 gulp.task('locator-scripts', function() {
   gulp.src('dev/clinic-locator/locator-scripts.js')
+    .pipe(uglify())
     .pipe(rename('local.js'))
     .pipe(gulp.dest('dist/clinic-locator'))
 });

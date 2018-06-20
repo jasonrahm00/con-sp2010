d4c4d6f79b0006ba2aef66b0181d2164cb61d288
Jason Rahm <jason.rahm@ucdenver.edu>
2018-06-20 14:16:39 -0600

    caml query used to load data

--- dev/faculty-directory/bios/faculty-bio-scripts.js
+++ dev/faculty-directory/bios/faculty-bio-scripts.js
@@ -1,42 +1,24 @@
 var defaultFaculty = {
-  Bio: "<div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p><p>Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor.</p></div>",
-  Biosketch: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Documents/cv-sample.pdf",
-  CV: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Documents/cv-sample.pdf",
-  Degree: "PhD, MS",
-  Education: "<ul><li>PhD, Harvard</li><li>MS, Yale</li><li>BSN, CU Boulder</li></ul>",
-  EMail: "somewhere@email.com",
-  Headshot: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/PublishingImages/default-profile.png",
-  Name: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/fred-faculty.aspx, Faculty, Fred",
-  Office: "12345",
-  Phone: "555-555-5555",
-  Specialization: "<ul><li><a href='http://www.ucdenver.edu/academics/colleges/nursing/programs-admissions/graduate-programs/graduate-specialties/healthcareinformatics/Pages/default.aspx'>Health Care Informatics</a></li><li>Research</li></ul>",
-  Title: "Assistant Professor"
+  bio: "<div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p><p>Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor.</p></div>",
+  biosketch: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Documents/cv-sample.pdf",
+  cv: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Documents/cv-sample.pdf",
+  degree: "PhD, MS",
+  education: "<ul><li>PhD, Harvard</li><li>MS, Yale</li><li>BSN, CU Boulder</li></ul>",
+  email: "somewhere@email.com",
+  headshot: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/PublishingImages/default-profile.png",
+  name: "Faculty, Fred",
+  office: "12345",
+  page: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/fred-faculty.aspx",
+  phone: "555-555-5555",
+  specialty: "<ul><li><a href='http://www.ucdenver.edu/academics/colleges/nursing/programs-admissions/graduate-programs/graduate-specialties/healthcareinformatics/Pages/default.aspx'>Health Care Informatics</a></li><li>Research</li></ul>",
+  title: "Assistant Professor"
 };
 
 function stripSpaces(strng) {
-  return strng.replace(/[\u200B]/g, '');
+  return strng !== null ? strng.replace(/[\u200B]/g, '') : null;
 }
 
-function facultyDataObj(obj) {
-  return {
-          bio: stripSpaces(obj.Bio),
-          biosketch: obj.Biosketch,
-          cv: obj.CV,
-          degree: obj.Degree,
-          education: obj.Education,
-          email: obj.EMail,
-          headshot: obj.Headshot,
-          firstName: obj.Name.split(',')[2],
-          lastName: obj.Name.split(',')[1],
-          office: obj.Office,
-          phone: obj.Phone,
-          specialization: obj.Specialization,
-          title: obj.Title,
-          url: obj.Name.split(',')[0]
-        };
-}
-
-angular.module("facultyBios", [])
+angular.module("facultyBio", [])
 .filter("renderHTMLCorrectly", function($sce) {
   return function(stringToParse) {
     return $sce.trustAsHtml(stringToParse);
@@ -46,48 +28,85 @@ angular.module("facultyBios", [])
 
   "use strict";
 
-  // getData method to be called from controller
-    // Returns promise with announcement data object array upon success
-  this.getData = function(url) {
+  this.getData = function(queryObj) {
 
-    var deferred = $q.defer();
+    var deferred = $q.defer(),
+        clientContext = new SP.ClientContext(queryObj.listUl),
+        web = clientContext.get_web(),
+        list = web.get_lists().getByTitle(queryObj.listName);
 
-    // Get function that retrieves data from Internal Annoucements list
-    return $http.get(url)
-      .then(function(response) {
-        deferred.resolve(response);
-        return deferred.promise;
-    }, function(error) {
-        deferred.reject(error);
-        return deferred.promise;
-    });
+    var items = list.getItems(SP.CamlQuery.createAllItemsQuery());
+
+    clientContext.load(items);
+    clientContext.executeQueryAsync(onQuerySucceed, onQueryFail);
+
+    function onQuerySucceed() {
+      var data = [],
+          itemEnumerator = items.getEnumerator();
+
+      while (itemEnumerator.moveNext()) {
+        var item = itemEnumerator.get_current(),
+            obj = {};
+
+        if (item.get_item("Name").get_url() == queryObj.currentPage) {
+          obj["page"] = item.get_item("Name").get_url();
+          obj["name"] = item.get_item("Name").get_description();
+          obj["title"] = item.get_item("Title");
+          obj["headshot"] = item.get_item("Headshot");
+          obj["bio"] = stripSpaces(item.get_item("Bio"));
+          obj["biosketch"] = item.get_item("Biosketch");
+          obj["cv"] = item.get_item("CV");
+          obj["degree"] = item.get_item("Degree");
+          obj["education"] = stripSpaces(item.get_item("Education"));
+          obj["email"] = item.get_item("EMail");
+          obj["office"] = item.get_item("Office");
+          obj["phone"] = item.get_item("PrimaryNumber");
+          obj["specialty"] = stripSpaces(item.get_item("Specialty"));
+
+          data.push(obj);
+
+          break;
+        }
+
+      }
+
+      deferred.resolve(data);
+
+    }
+
+    function onQueryFail() {
+      deferred.reject();
+    }
+
+    return deferred.promise;
+
   };
 
 })
 .controller("mainController", function($scope, dataService){
 
-  var smallList = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/_vti_bin/listdata.svc/FacultyDirectory?$orderby=Name asc",
-      fullList = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/_vti_bin/listdata.svc/Faculty?$orderby=Name asc",
-      michaelRice = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/_vti_bin/listdata.svc/Faculty?$filter=(LastName eq 'Rice') and (FirstName eq 'Michael')",
-      news = "http://www.ucdenver.edu/academics/colleges/nursing/about-us/news/_vti_bin/ListData.svc/NewsItems?$expand=Category,Faculty&$orderby=PublishDate desc";
+  $scope.currentPage = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/m_rice.aspx";
+  $scope.currentPage = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/c_jankowski.aspx";
 
-  $scope.url = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/m_rice.aspx";
+  var queryObj = {
+        listUrl: "/academics/colleges/nursing/faculty-staff/faculty/",
+        listName: "Faculty",
+        currentPage: $scope.currentPage
+      };
 
-  dataService.getData(fullList).then(function(response) {
-    var results = response.data.d.results;
+  jQuery(document).ready(function($) {
+    ExecuteOrDelayUntilScriptLoaded(loadData, "sp.js");
+  });
 
-    console.log(results);
+  function loadData() {
+    dataService.getData(queryObj).then(function(response) {
 
-    results.forEach(function(elem) {
-      if (elem.Name.split(',')[0] == $scope.url) {
-        $scope.data = facultyDataObj(elem);
-      } else {
-        $scope.data = facultyDataObj(defaultFaculty);
-      }
-    });
+      $scope.data = response.length > 0 ? response[0] : defaultFaculty;
 
-  }, function(error) {
-    console.log(error);
-  })
+    }, function(error) {
+      console.log(error);
+    })
+  }
+
 
 });

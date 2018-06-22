bad5dea2ae74177c8cc96047612a0e244ad55e7c
Jason Rahm <jason.rahm@ucdenver.edu>
2018-06-22 08:48:54 -0600

    live data and template selector added

--- dev/faculty-directory/bios/faculty-bio-scripts.js
+++ dev/faculty-directory/bios/faculty-bio-scripts.js
@@ -1,5 +1,5 @@
 var defaultFaculty = {
-  bio: "<div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</p><p>Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor.</p></div>",
+  bio: "<div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac fringilla orci. Fusce at ornare mi, sed vestibulum nibh. Etiam efficitur libero at lorem semper, in suscipit justo vehicula. Phasellus sollicitudin metus volutpat sem sodales posuere. Cras commodo commodo augue, eget mollis dolor lobortis ac. Sed imperdiet dui non leo fermentum pretium. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean dui tellus, tempor at bibendum vitae, tempus id felis. Donec condimentum id orci ut ornare. Nunc eu ligula tincidunt, rhoncus tortor eu, condimentum turpis. Nam dictum mollis suscipit. Praesent orci nunc, pharetra sit amet blandit quis, dapibus ut mi. Quisque vel varius arcu. Integer ultrices hendrerit neque, nec blandit quam elementum quis.</p><p>Sed eget ex quis enim ultrices rutrum. Sed dolor orci, efficitur in vulputate at, tincidunt vitae leo. Phasellus in rhoncus ligula. Nunc blandit viverra facilisis. Curabitur eget vehicula elit. Integer eget velit elit. Aliquam viverra nisi est, sit amet rutrum nisi convallis nec. Duis eget fermentum ex, sit amet maximus est. Praesent eleifend dolor ac bibendum rutrum. Mauris tristique eu enim sed semper. Morbi ac pharetra elit. In in tempor lacus. Mauris porttitor interdum bibendum. Aenean et felis consequat, consectetur ex at, sollicitudin diam. Nullam tempor aliquam nisi, et pretium risus lacinia et.</p><p>Donec semper accumsan ligula a dapibus. Sed vitae finibus mauris, et porta tellus. Nullam sagittis, tortor in ultrices suscipit, ligula orci aliquet augue, id varius lorem libero eget felis. Aliquam at augue augue. Pellentesque pulvinar sed magna vitae aliquet. Nullam sed erat est. Vivamus congue odio id ex auctor faucibus. Vestibulum posuere viverra ornare. Donec vitae lobortis massa, ut suscipit risus. Suspendisse commodo facilisis orci, et commodo risus posuere sit amet. Etiam interdum purus elit, auctor scelerisque ante sodales a. Integer sit amet nulla ipsum. Sed facilisis orci ex, varius scelerisque arcu iaculis et.</p><p>Cras id est eu turpis pellentesque gravida id vel elit. Quisque bibendum est non bibendum convallis. Sed egestas risus vel elit hendrerit dapibus. Aliquam et fermentum neque. Aliquam lacinia auctor lorem eget laoreet. Integer vel magna neque. Proin ex eros, tempor ut est et, tincidunt auctor sapien. Morbi in fermentum ex. Vivamus at lorem ut diam dictum tempor. Pellentesque odio dolor, ullamcorper et tortor ac, luctus commodo lorem. Praesent id diam eget orci facilisis hendrerit. Nunc porttitor ligula ac arcu facilisis euismod. Morbi mattis leo et nisl cursus, eget lacinia diam semper. Donec dapibus porttitor lorem, eget luctus ipsum luctus non. Donec ut tellus vel purus rutrum viverra at nec sapien. Duis tempor, mi sed ornare interdum, magna ex porttitor neque, fermentum convallis dolor mauris nec elit.</p><p>In ornare augue ut nisi faucibus, posuere dictum leo mollis. Duis pretium diam et feugiat molestie. Aenean vel nibh consequat, posuere mauris ac, rutrum risus. Praesent sit amet justo eleifend, ullamcorper eros vel, accumsan ligula. Mauris sodales mollis massa. Aenean molestie eros nec gravida eleifend. In sed nisi consequat, pellentesque justo cursus, facilisis tellus. Integer tristique massa id consequat posuere. Vestibulum eget finibus.</p></div>",
   biosketch: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Documents/cv-sample.pdf",
   cv: "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Documents/cv-sample.pdf",
   degree: "PhD, MS",
@@ -24,18 +24,18 @@ angular.module("facultyBio", [])
     return $sce.trustAsHtml(stringToParse);
   }
 })
-.service("dataService", function($http, $q){
+.service("dataService", function($http, $q) {
 
-  "use strict";
+  var listUrl = "/academics/colleges/nursing/faculty-staff/faculty/",
+      listName = "Faculty";
 
-  this.getData = function(queryObj) {
+  this.getData = function(currentPage) {
 
     var deferred = $q.defer(),
-        clientContext = new SP.ClientContext(queryObj.listUl),
+        clientContext = new SP.ClientContext(listUrl),
         web = clientContext.get_web(),
-        list = web.get_lists().getByTitle(queryObj.listName);
-
-    var items = list.getItems(SP.CamlQuery.createAllItemsQuery());
+        list = web.get_lists().getByTitle(listName),
+        items = list.getItems(SP.CamlQuery.createAllItemsQuery());
 
     clientContext.load(items);
     clientContext.executeQueryAsync(onQuerySucceed, onQueryFail);
@@ -48,7 +48,7 @@ angular.module("facultyBio", [])
         var item = itemEnumerator.get_current(),
             obj = {};
 
-        if (item.get_item("Name").get_url() == queryObj.currentPage) {
+        if (item.get_item("Name").get_url() == currentPage) {
           obj["page"] = item.get_item("Name").get_url();
           obj["name"] = item.get_item("Name").get_description();
           obj["title"] = item.get_item("Title");
@@ -83,30 +83,114 @@ angular.module("facultyBio", [])
   };
 
 })
-.controller("mainController", function($scope, dataService){
+.service("newsService", function($http, $q) {
+  var listUrl = "/academics/colleges/nursing/about-us/news/",
+      listName = "news-items";
 
-  $scope.currentPage = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/m_rice.aspx";
-  $scope.currentPage = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/c_jankowski.aspx";
+  this.getNews = function(currentPage) {
+    var deferred = $q.defer(),
+        clientContext = new SP.ClientContext(listUrl),
+        web = clientContext.get_web(),
+        list = web.get_lists().getByTitle(listName),
+        items = list.getItems(SP.CamlQuery.createAllItemsQuery());
 
-  var queryObj = {
-        listUrl: "/academics/colleges/nursing/faculty-staff/faculty/",
-        listName: "Faculty",
-        currentPage: $scope.currentPage
-      };
+    clientContext.load(items);
+    clientContext.executeQueryAsync(onQuerySucceed, onQueryFail);
+
+    function onQuerySucceed() {
+      var data = [],
+          itemEnumerator = items.getEnumerator();
+
+      while (itemEnumerator.moveNext()) {
+
+        var item = itemEnumerator.get_current(),
+            obj = {},
+            facultyArray = item.get_item("Faculty_x0020_Pages") !== null ? item.get_item("Faculty_x0020_Pages").split(",") : null;
+
+        if (facultyArray !== null) {
+          facultyArray.forEach(function(elem) {
+            elem = elem.replace(/\s+/g, ' ').trim();
+            if(currentPage.indexOf(elem) > -1) {
+              obj["title"] = item.get_item("Name").get_description();
+              obj["link"] = item.get_item("Name").get_url();
+              obj["published"] = new Date(item.get_item("Publish_x0020_Date"));
+              obj["category"] = item.get_item("Category");
+              obj["promoted"] = item.get_item("Promoted");
+
+              data.push(obj);
+            }
+          });
+        }
+      }
+
+      deferred.resolve(data.reverse().slice(0,5));
+
+    }
+
+    function onQueryFail() {
+      deferred.reject();
+    }
+
+    return deferred.promise;
+  }
+
+})
+.controller("mainController", function($scope, dataService, newsService){
+
+  $scope.layout = "default";
+  $scope.currentPage = window.location.href;
 
   jQuery(document).ready(function($) {
     ExecuteOrDelayUntilScriptLoaded(loadData, "sp.js");
   });
 
   function loadData() {
-    dataService.getData(queryObj).then(function(response) {
 
+    // Get fac data
+    dataService.getData($scope.currentPage).then(function(response) {
       $scope.data = response.length > 0 ? response[0] : defaultFaculty;
+    }, function(error) {
+      console.log(error);
+    });
+
+    // Get news
+    newsService.getNews($scope.currentPage).then(function(response) {
+      var x = [];
+
+      response.forEach(function(elem) {
+        if (elem.promoted === true && !$scope.promotedNews) {
+          $scope.promotedNews = elem;
+        } else {
+          x.push(elem);
+        }
+      });
+
+      $scope.news = x;
 
     }, function(error) {
       console.log(error);
-    })
+    }) ;
+
   }
+
+  $scope.$watch("layout", function(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      $scope.data = [];
+      $scope.news = null;
+      $scope.promotedNews = undefined;
+
+      if(newVal === "rice") {
+        $scope.currentPage = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/m_rice.aspx";
+      } else if (newVal === "jankowski") {
+        $scope.currentPage = "http://www.ucdenver.edu/academics/colleges/nursing/faculty-staff/faculty/Pages/c_jankowski.aspx";
+      } else {
+        $scope.currentPage = window.location.href;
+      }
+
+      loadData();
+
+    }
+  });
 
 
 });

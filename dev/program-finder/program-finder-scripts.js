54989779d7a0b4c7b5830d5416e33524855af03d
Jason Rahm <jason.rahm@ucdenver.edu>
2018-11-07 13:30:53 -0700

    sortOrder.level entries updated per additional stakeholder feedback

--- dev/program-finder/program-finder-scripts.js
+++ dev/program-finder/program-finder-scripts.js
@@ -15,8 +15,7 @@ var sortOrder = {
     "RN (ADN) License",
     "Non-Nursing Bachelor's",
     "ADN and Non-Nursing Bachelor's",
-    "Nursing Bachelor's and RN License",
-    "Nursing Bachelor's",
+    "Nursing Bachelor's and Active RN License",
     "BSN and Advanced Degree",
     "Nursing Master's"
   ],

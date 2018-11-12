ea19c121632e5604c824a804b965406230d64967
Jason Rahm <jason.rahm@ucdenver.edu>
2018-11-12 12:03:57 -0700

    styling added to admin page widget

--- dev/directory/administration/local.js
+++ dev/directory/administration/local.js
@@ -6,16 +6,41 @@ $(document).ready(function() {
 
   function createRowObj(x) {
     var obj = {
-      headshot: x[0].innerHTML ? x[0].innerHTML : null,
-      lastName: x[1].innerHTML ? x[1].innerHTML : null,
-      firstName: x[2].innerHTML ? x[2].innerHTML : null,
-      degree: x[3].innerHTML ? x[3].innerHTML : null,
-      email: x[4].innerHTML ? x[4].innerHTML : null,
-      phone: x[5].innerHTML ? x[5].innerHTML : null,
-      title: x[6].innerHTML ? x[6].innerHTML : null
+      headshot: x[2].innerHTML ? x[2].innerHTML : null,
+      lastName: x[3].innerHTML ? x[3].innerHTML : null,
+      firstName: x[4].innerHTML ? x[4].innerHTML : null,
+      degree: x[5].innerHTML ? x[5].innerHTML : null,
+      email: x[6].innerHTML ? x[6].innerHTML : null,
+      phone: x[7].innerHTML ? x[7].innerHTML : null,
+      title: x[8].innerHTML ? x[8].innerHTML : null,
+      page: x[9].textContent ? x[9].textContent : null
     };
+    return(obj);
+  }
 
-    console.log(obj);
+  function linkToBio(x) {
+    if (!x.page) {
+      return '<strong>' + x.lastName + ', ' + x.firstName + (x.degree ? ', ' + x.degree : '') + '</strong><br>';
+    } else {
+      return '<a href="' + x.page + '"><strong>' + x.lastName + ', ' + x.firstName + (x.degree ? ', ' + x.degree : '') + '</strong><span class="page-link"></span></a><br>';
+    }
+  }
+
+  function createRowContent(x) {
+    var rowContent = '<td colspan="1">';
+          rowContent += x.headshot ? x.headshot : '';
+        rowContent += '</td>';
+        rowContent += '<td colspan="3">';
+          rowContent += linkToBio(x);
+          rowContent += '<span>' + x.title + '</span>';
+        rowContent += '</td>';
+        rowContent += '<td colspan="3">';
+          rowContent += '<ul>';
+            rowContent += '<li>Phone: ' + x.phone + '</li>' ;
+            rowContent += '<li>Email: ' + x.email + '</li>';
+          rowContent += '</ul>';
+        rowContent += '</td>';
+    return rowContent;
   }
 
   while (currentTable < tableCount) {
@@ -25,9 +50,14 @@ $(document).ready(function() {
       if (index === 0) {
         return;
       } else {
-        createRowObj($(value).children())
+        var person = createRowObj($(value).children());
+        $(value).html(createRowContent(person));
+        $(value).addClass('directory-row');
       }
-    })
+    });
+
+    $(adminTables[currentTable]).find('tr.ms-viewheadertr.ms-vhltr').remove();
+    $(adminTables[currentTable]).show();
 
     currentTable++;
   };
